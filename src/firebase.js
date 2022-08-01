5af037e034b3fffc7fb7938581c019fd5a9d748d
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-01 22:36:22 +0300

    creating react app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAepswD_i7elnbbOrt4YNedw4J6FjxaEPA",
+  authDomain: "e-clone-6ce47.firebaseapp.com",
+  projectId: "e-clone-6ce47",
+  storageBucket: "e-clone-6ce47.appspot.com",
+  messagingSenderId: "899587004039",
+  appId: "1:899587004039:web:0315dcdf7fbab976528a22",
+};
