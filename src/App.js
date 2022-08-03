05683549eda740ce14bc9ed18597bae177a64da3
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-03 21:30:21 +0300

    Add routing and checkout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 import Navbar from "./components/header/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
+import Checkout from "./components/checkout/Checkout";
 
 function App() {
   return (
-    <div className="app">
-      {/* Header */}
-      <Navbar />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
