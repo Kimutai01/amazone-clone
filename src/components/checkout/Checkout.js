a2bf37994b825eb44ad7c75393f1e038fc1b2061
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-03 22:27:43 +0300

    Add subtotal from react currency format

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import SubTotal from "./SubTotal";
 
 const Checkout = () => {
   return (
@@ -16,7 +17,6 @@ const Checkout = () => {
       </div>
       <div className="checkout__right">
         <SubTotal />
-        <h2>The total will go here</h2>
       </div>
     </div>
   );
