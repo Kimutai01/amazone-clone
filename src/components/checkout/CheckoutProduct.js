bdfd5473e43045f17b9ad1705724e2bbebb43a73
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-05 14:46:16 +0300

    Create checkout product component

---
+++ src/components/checkout/CheckoutProduct.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./CheckoutProduct.css";
+
+const CheckoutProduct = ({ id, image, title, price, rating }) => {
+  return (
+    <div className="checkoutProduct">
+      <img src={image} alt="product" />
+      <div className="checkoutProduct__input">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+        <button>Remove from basket</button>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutProduct;
