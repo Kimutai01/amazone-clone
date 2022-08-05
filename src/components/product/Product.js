d225e787bba1a573932e8a028453e27a1ccc962a
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-05 12:53:37 +0300

    Add the basket to the data layer

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useStateValue } from "../../StateProvider";
 import "./Product.css";
 
-const Product = ({ title, image, price, rating }) => {
+const Product = ({ id, title, image, price, rating }) => {
+  const [{ basket }, dispatch] = useStateValue();
+  console.log(basket);
+  const addToBasket = () => {
+    // dispatch an item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -19,7 +35,7 @@ const Product = ({ title, image, price, rating }) => {
         </div>
       </div>
       <img src={image} alt="the lean startup" />
-      <button>Add to basket</button>
+      <button onClick={addToBasket}>Add to basket</button>
     </div>
   );
 };
